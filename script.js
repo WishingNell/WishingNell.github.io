f00793a2a46dc107f4630c4b89a1696fa43363ed
WishingNell <nellyorkcreative@gmail.com>
2022-05-29 13:30:37 -0700

    added CSS and JS

---
+++ script.js
@@ -0,0 +1,14 @@
+// Old JS lol 
+function scream(phrase) {
+	console.log('I scream: ' + phrase)
+}
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    scream('AHHHHHHHH')
+})
+
+// New JS would be
+const scream2 = (phrase) => {
+	console.log('I scream: ' + phrase)
+}
